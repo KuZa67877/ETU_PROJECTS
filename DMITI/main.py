5cecc58f342d2efeb4b1947ff6fa7379056c7b79
Egor Kuzmihykh <egor67877@gmail.com>
2024-01-11 10:10:52 +0300

    little fixes

--- DMITI/main.py
+++ DMITI/main.py
@@ -36,7 +36,7 @@ def statistic(probability_a, probability_b):
    total_points = points.sum()
    print(f"Количество выигранных очков игроком А - {total_points}")
    avg_points = points.mean()
-   print(f"Среднее количество очков за игру - {avg_points}")
+   print(f"Среднее значение - {avg_points}")
    occ = np.array([probability_a * probability_b, (1 - probability_b) * probability_a, (1 - probability_a) * probability_b, (1 - probability_a) * (1 - probability_b)])
    game_scores = game_field.flatten()
    expected_score = math_waiting(game_scores, occ)
@@ -46,5 +46,5 @@ def statistic(probability_a, probability_b):
    disp = dispersion(game_scores, occ)
    print(f"Дисперсия - {disp}")
    theoretical_skill_level = np.sqrt(disp)
-   print(f"СКО - {theoretical_skill_level}")
+   print(f"Среднее квадратическое отклонение - {theoretical_skill_level}")
 
--- DMITI/second_and_third_task.py
+++ DMITI/second_and_third_task.py
@@ -1,4 +1,5 @@
 from main import statistic
-
+print("Task #2:")
 statistic(0.5, 0.5)
+print("Task #3:")
 statistic(0.5, 0.25)
